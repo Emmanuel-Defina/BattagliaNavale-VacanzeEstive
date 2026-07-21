37dd12922649847abbf91a701fdf70fb5e6d781c
Emmanuel-Defina <emmanueldefina2017@gmail.com>
2026-07-22 01:35:35 +0200

    inserimento grafica ed inizio logica di gioco in js

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
 "option-strict"
 
+let Righe = 10;
+ let Colonne = 10;ù
+
+ let divGriglia;
+ let divCelle;
+
+ window.onload = function ()
+ {
+    divGriglia = document.getElementById("griglia");
+
+    divGriglia.classList.add("griglia");
+
+    divCelle = [];
+    let g = 0;
+
+    for(let i = 0; i < Righe; i++) {
+        divCelle[i] = [];
+
+        for (let j = 0; j < Colonne; j++) {
+            let cella = document.createElement("div");
+            cella.id = "c" + g;
+            g = g + 1;
+            cella.classList.add("cella");
+            divGriglia.append(cella);
+            divCelle[i][j] = cella
+        }
+    }
+ }
+
+ function avviaGioco()
+ {
+    for(let i = 0; i < 6; i++) 
+        {
+            
+        }
+ }
