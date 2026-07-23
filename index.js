33b4a44964c91df28c1475ead062feacfaf91526
Emmanuel-Defina <emmanueldefina2017@gmail.com>
2026-07-24 01:45:44 +0200

    inserimento controlli per posizionamento navi e parte grafica di colpita o mancata

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 "option-strict"
 
-let Righe = 10;
- let Colonne = 10;ù
+ let Righe = 10;
+ let Colonne = 10;
 
- let divGriglia;
- let divCelle;
+ const nNavi = [2,3,4,5,6];
+
+let matNavi = [];
+
 
  window.onload = function ()
  {
+
+    let divGriglia;
+    let divCelle;
+
     divGriglia = document.getElementById("griglia");
 
     divGriglia.classList.add("griglia");
@@ -20,19 +26,140 @@ let Righe = 10;
 
         for (let j = 0; j < Colonne; j++) {
             let cella = document.createElement("div");
-            cella.id = "c" + g;
-            g = g + 1;
+            cella.id = i + "-" + j;
             cella.classList.add("cella");
+            cella.addEventListener("click", function() {
+                controlloVittoria(cella.id);
+            });
             divGriglia.append(cella);
             divCelle[i][j] = cella
+        }
+    }
+
+    for(let i = 0; i < Righe; i++)
+    {
+        matNavi[i] = [];
+
+        for(let j = 0; j < Colonne; j++)
+        {
+            matNavi[i][j] = 0;
         }
     }
  }
 
  function avviaGioco()
  {
-    for(let i = 0; i < 6; i++) 
+    
+    let bntGioco = document.getElementById("Start");
+
+    bntGioco.disabled = true;
+
+    let lungh = 0;
+
+    let modDiPos; 
+
+    let posX;
+    let posY;
+
+    for(let i = 0; i < nNavi.length; i++)
+    {
+        lungh = nNavi[i];
+
+        let rigOcol = Math.floor(Math.random() * 2);
+
+        if(rigOcol == 0)
         {
-            
+            modDiPos = "riga";
         }
+        else
+        {
+            modDiPos = "colonna";
+        }
+
+        do
+        {
+
+        posX = Math.floor(Math.random() * Righe);
+        posY = Math.floor(Math.random() * Colonne);
+
+        }while(!controlloPosizione(lungh, posX, posY, modDiPos));
+
+        posizionaNave(lungh, posX, posY, modDiPos);
+
+    }
+
+    console.log("Matrice delle navi:");
+    console.table(matNavi);
+ }
+
+ function controlloPosizione(lungh, posX, posY, modDiPos)
+ {
+    if(modDiPos === "riga")
+    {
+        if(posX + lungh > Righe)
+        {
+            return false;
+        }
+
+        for(let k = 0; k < lungh; k++)
+        {
+            if(matNavi[posX + k][posY] === 1)
+            {
+                return false;
+            }
+        }
+    }
+    else
+    {
+        if(posY + lungh > Colonne)
+        {
+            return false;
+        }
+
+        for(let k = 0; k < lungh; k++)
+        {
+            if(matNavi[posX][posY + k] === 1)
+            {
+                return false;
+            }
+        }
+    }
+
+    return true;
+ }
+
+ function posizionaNave(lungh, posX, posY, modDiPos)
+ {
+    if(modDiPos === "riga")
+    {
+        for(let k = 0; k < lungh; k++)
+        {
+            matNavi[posX + k][posY] = 1;
+        }
+    }
+    else
+    {
+        for(let k = 0; k < lungh; k++)
+        {
+            matNavi[posX][posY + k] = 1;
+        }
+    }
+ }
+
+ function controlloVittoria(idCella)
+ {
+    let parti = idCella.split("-");
+    let riga = Number(parti[0]);
+    let colonna = Number(parti[1]);
+
+    let cella = document.getElementById(idCella);
+
+    if(matNavi[riga][colonna] === 1)
+    {
+        cella.style.backgroundColor = "green";
+    }
+    else
+    {
+        cella.style.backgroundColor = "red";
+    }
  }
